19db907c8acc1a1506c4466452304dfcd35d90ec
ltlaitoff <ltlaitoff@gmail.com>
2023-05-28 15:50:57 +0300

    styles home-view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,15 @@
 <template>
-	<div class="backGroundIkon">
+	<div class="pt-4 flex flex-wrap justify-between w-full bg-gray-50">
 		<template v-if="champions !== null">
-			<router-link
-				v-for="champion in champions"
-				:key="champion.key"
-				:to="'/champion/' + champion.id"
-				class="ikonChamp"
-			>
-				<div class="textForIkon">
-					<img
+			<router-link v-for="champion in champions" :key="champion.key" :to="'/champion/' + champion.id" class="ikonChamp">
+				<div class="py-4 px-2 rounded w-[170px] flex flex-col items-center text-center gap-y-2 hover:bg-sky-200">
+					<img class="w-24"
 						:src="`http://ddragon.leagueoflegends.com/cdn/${champion.version}/img/champion/${champion.image.full}`"
-						alt=""
-					/>
-					<div>{{ champion.name }}</div>
-					<div>{{ champion.tags.join(', ') }}</div>
+						:alt="champion.name" />
+					<div class="">
+						<div class="text-md text-slate-900">{{ champion.name }}</div>
+						<div class="text-base text-slate-700">{{ champion.tags.join(', ') }}</div>
+					</div>
 				</div>
 			</router-link>
 		</template>
@@ -38,4 +34,3 @@ export default {
 }
 </script>
 
-<style src="../style/views/homeView.css"></style>
